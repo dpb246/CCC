54aacc1aee6288316ce9121139360fab8709e0ad
dpb246 <dpb246@users.noreply.github.com>
2018-08-19 14:51:45 -0400

    Fixed TLE

--- 2000/senior4.py
+++ 2000/senior4.py
@@ -1,7 +1,47 @@
+"""
+Full marks
+"""
 distance = int(input())
 clubcount = int(input())
-clubs = sorted([int(input()) for i in range(clubcount)])[::-1]
+clubs = [int(input()) for i in range(clubcount)]
 
+moves = [0 for i in range(distance+1)]
+for i in range(1,distance+1):
+    minimum = 1000000
+    for c in clubs:
+        if c <= i:
+            if (1+moves[i-c] < minimum):
+                minimum = 1+moves[i-c]
+    moves[i] = minimum
+if moves[-1] == 1000000:
+    print("Roberta acknowledges defeat.")
+else:
+    print("Roberta wins in {} strokes.".format(moves[-1]))
+
+
+"""
+Recursive style: TLE on test 5
+def number(distance):
+    if distance == 0:
+        return 0
+    else:
+        minimum = 1000000
+        for c in clubs:
+            if c <= distance:
+                moves = 1 + number(distance-c)
+                if moves < minimum:
+                    minimum = moves
+        return minimum
+
+value = number(distance)
+if value == 1000000:
+    print("Roberta acknowledges defeat.")
+else:
+    print("Roberta wins in {} strokes.".format(value))
+"""
+
+"""
+Bad Greedy style, doesn't solve all cases
 n = 0
 i = 0
 while True:
@@ -17,3 +57,4 @@ while True:
         print("Roberta wins in {} strokes.".format(n))
         break
 
+"""
